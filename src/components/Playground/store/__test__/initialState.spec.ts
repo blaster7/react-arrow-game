093acc131a97ee29fd82b693549d4c61f22d3131
Александр <aleksandr.titov@kitesolution.ru>
2024-05-20 00:30:12 +0300

    vitest

---
+++ src/components/Playground/store/__test__/initialState.spec.ts
@@ -0,0 +1,11 @@
+import playgroundReducer, { initialState } from "../slices"
+
+describe("initial state", () => {
+  it("check initial state", () => {
+    const state = playgroundReducer(undefined, {
+      type: "unknown",
+    })
+
+    expect(state).toEqual(initialState)
+  })
+})
